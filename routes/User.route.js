b6684537bda8c9d5bec785cc58ba91ed3f5f7d0a
varun <varuvrk02@gmail.com>
2023-11-08 19:46:55 +0530

    user login route working successfully

--- routes/User.route.js
+++ routes/User.route.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const UserModel = require('../model/User.model');
+const jwt = require('jsonwebtoken');
+
 
 const userRouter = express.Router();
 
+
+// signup route
 userRouter.post('/signup', (req, res) => {
     const { name, email, password } = req.body;
     try {
@@ -23,6 +27,34 @@ userRouter.post('/signup', (req, res) => {
     }
 })
 
+
+
+// login route
+userRouter.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await UserModel.findOne({ email });
+        if (user) {
+            bcrypt.compare(password, user.password, function (err, result) {
+                // result == true
+                if (result) {
+                    const generatedToken = jwt.sign({ username: user.name, userId: user._id }, 'questHub123');
+                    res.status(200).send({ msg: "Login Successful !!", token: generatedToken })
+                }
+                else{
+                    res.status(200).send({msg: "Wrong credentials"})
+                }
+            });
+        }
+        else {
+            res.status(200).send({msg: "Wrong credentials"})
+        }
+    } catch (error) {
+       res.status(400).send({'err': error.message})
+    }
+
+})
 module.exports = {
     userRouter
 }
